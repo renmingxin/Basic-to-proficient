e66c0e221d46340e996971ad4775ead6519a8fc1
rmx916586942 <916586942@qq.com>
2019-07-02 09:51:15 +0800

    仿写客户端和服务器通信

---
+++ node/仿写client与server通信/client.js
@@ -0,0 +1,4 @@
+var net  = require("net");
+
+//socket === 通信管道
+var socket = net.connect(12306,"127.0.0.1");
---
+++ node/仿写client与server通信/server.js
@@ -0,0 +1,5 @@
+var net  = require("net");
+
+var server = net.createServer();
+console.log(server)
+// server.listen(12306,"127.0.0.1");
